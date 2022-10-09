efc5bee426d68fcdadfb537c83bc7c42db85b1a0
Juan Jose Henao <j.henao@crisolweb.net>
2022-10-09 15:48:51 -0500

    Feat: Integracion de secciones en vistas parciales

---
+++ PuntoDeVenta.AplicacionWeb/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PuntoDeVenta.AplicacionWeb.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PuntoDeVenta.AplicacionWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PuntoDeVenta.AplicacionWeb.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult usuario()
+        {
+            return View();
+        }
+    }
+}
