a7d5bc09f6a8fb7b2c7eda1ec0158e6862308db5
Juan Jose Henao <j.henao@crisolweb.net>
2022-10-13 22:55:45 -0500

    Feat: Integración de la vista 'Mi Cuenta'

---
+++ PuntoDeVenta.AplicacionWeb/Controllers/MiCuentaController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PuntoDeVenta.AplicacionWeb.Controllers
+{
+    public class MiCuentaController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
