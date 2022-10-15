d4684fcfe86098a8c6bac8ba6f12727524639dff
Juan Jose Henao <j.henao@crisolweb.net>
2022-10-15 08:42:46 -0500

    Feat: Ajustes puntuales en los elementos de mi cuenta
    Feat-new: Nueva vista de Soporte

---
+++ PuntoDeVenta.AplicacionWeb/Controllers/ContactoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PuntoDeVenta.AplicacionWeb.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
