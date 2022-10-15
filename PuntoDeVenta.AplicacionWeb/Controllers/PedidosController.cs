b69bf2b328af80b637e77bf3be3016fa95c2e400
Juan Jose Henao <j.henao@crisolweb.net>
2022-10-15 16:10:11 -0500

    Feat: Se eliminaron vistas como 'Ventas' y se agrego en cambio, dos vistas llamadas 'Registro' y 'Pedidos'

---
+++ PuntoDeVenta.AplicacionWeb/Controllers/PedidosController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PuntoDeVenta.AplicacionWeb.Controllers
+{
+    public class PedidosController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
---
+++ PuntoDeVenta.AplicacionWeb/Controllers/RegistroDeVentasController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PuntoDeVenta.AplicacionWeb.Controllers
+{
+    public class RegistroDeVentasController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
